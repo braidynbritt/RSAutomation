60f2551b2259f772349859573ba26e392a549b2f
Braidyn Britt <75519537+braidynbritt@users.noreply.github.com>
2024-05-06 12:48:09 -0500

    Add files via upload

---
+++ install_helper.bat
@@ -0,0 +1,20 @@
+@ECHO off
+
+ECHO "Installing Notepad++"
+timeout /t 2 >NUL
+start winget install Notepad++.Notepad++
+ECHO "Installing Python3"
+timeout /t 2 >NUL
+start winget install Python.Python.3.12
+ECHO "Installing Pip"
+timeout/t 2 >NUL
+start python get-pip.py
+ECHO "Installing pyautogui"
+timeout/t 2 >NUL
+start pip install pyautogui
+ECHO "Installing simple_colors"
+timeout/t 2 >NUL
+start pip install simple_colors
+ECHO "Installing traceback"
+timeout/t 2 >NUL
+start pip install traceback
